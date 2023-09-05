fb0a9a1dfc3f40ccb9007b3b95561784aecd85bd
Your Name <you@example.com>
2023-09-05 20:59:06 -0300

    05-09-2023

--- app/Controllers/DestinoInter.php
+++ app/Controllers/DestinoInter.php
@@ -4,45 +4,45 @@ namespace App\Controllers;
 
 class DestinoInter extends BaseController
 {
-    public function index()
+    public function auro()
     {
         return view('aurora'); // Nombre de la vista sin extensión
     }
     public function cancu()
     {
-        return view('cancun'); // Nombre de la vista sin extensión
+        return view('cancun'); 
     }
     public function brasi()
     {
-        return view('brasil'); // Nombre de la vista sin extensión
+        return view('brasil'); 
     }
     public function newy()
     {
-        return view('newyork'); // Nombre de la vista sin extensión
+        return view('newyork'); 
     }
     public function francia()
     {
-        return view('paris'); // Nombre de la vista sin extensión
+        return view('paris'); 
     }
     public function espa()
     {
-        return view('madrid'); // Nombre de la vista sin extensión
+        return view('madrid'); 
     }
     public function grecia()
     {
-        return view('islas'); // Nombre de la vista sin extensión
+        return view('islas'); 
     }
     public function lond()
     {
-        return view('londres'); // Nombre de la vista sin extensión
+        return view('londres'); 
     }
     public function jap()
     {
-        return view('tokyo'); // Nombre de la vista sin extensión
+        return view('tokyo'); 
     }
     public function ita()
     {
-        return view('venecia'); // Nombre de la vista sin extensión
+        return view('venecia'); 
     }
     
-}
+}0+
